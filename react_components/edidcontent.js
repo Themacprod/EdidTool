d19447678e27f843cf1ddd2bc01659813892f004
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2017-07-10 10:36:14 -0400

    Moved manufacturer info to specific function (help to design other field)

--- react_components/edidcontent.js
+++ react_components/edidcontent.js
@@ -9,6 +9,52 @@ module.exports = React.createClass({
     componentWillMount: function() {
         this.edidParser = new EdidParser();
     },
+    manufacturerInfo: function() {
+        return React.DOM.table(null,
+            React.DOM.tbody(null,
+                React.DOM.tr(null,
+                    React.DOM.th(null, "Manufacturer & Product ID"),
+                    React.DOM.th(null, "")
+                ),
+                React.DOM.tr(null,
+                    React.DOM.td(null, "Manufacturer ID (PnPID):"),
+                    React.DOM.td({
+                        className: "edid-content-data"
+                    }, this.edidParser.getManufacturerId())
+                ),
+                React.DOM.tr(null,
+                    React.DOM.td(null, "Manufacturer name:"),
+                    React.DOM.td({
+                        className: "edid-content-data"
+                    }, this.edidParser.getManufacturerName())
+                ),
+                React.DOM.tr(null,
+                    React.DOM.td(null, "Product ID:"),
+                    React.DOM.td({
+                        className: "edid-content-data"
+                    }, this.edidParser.getProductCode())
+                ),
+                React.DOM.tr(null,
+                    React.DOM.td(null, "Serial Number:"),
+                    React.DOM.td({
+                        className: "edid-content-data"
+                    }, this.edidParser.getSerialNumber())
+                ),
+                React.DOM.tr(null,
+                    React.DOM.td(null, "Week:"),
+                    React.DOM.td({
+                        className: "edid-content-data"
+                    }, this.edidParser.getManufactureWeek())
+                ),
+                React.DOM.tr(null,
+                    React.DOM.td(null, "Year:"),
+                    React.DOM.td({
+                        className: "edid-content-data"
+                    }, this.edidParser.getManufactureYear())
+                )
+            )
+        );
+    },
     render: function() {
         this.edidParser.setEdidData(this.props.edid);
         this.edidParser.parse();
@@ -16,50 +62,7 @@ module.exports = React.createClass({
         return React.DOM.div({
                 className: "edid-content"
             },
-            React.DOM.table(null,
-                React.DOM.tbody(null,
-                    React.DOM.tr(null,
-                        React.DOM.th(null, "Manufacturer & Product ID"),
-                        React.DOM.th(null, "")
-                    ),
-                    React.DOM.tr(null,
-                        React.DOM.td(null, "Manufacturer ID (PnPID):"),
-                        React.DOM.td({
-                            className: "edid-content-data"
-                        }, this.edidParser.getManufacturerId())
-                    ),
-                    React.DOM.tr(null,
-                        React.DOM.td(null, "Manufacturer name:"),
-                        React.DOM.td({
-                            className: "edid-content-data"
-                        }, this.edidParser.getManufacturerName())
-                    ),
-                    React.DOM.tr(null,
-                        React.DOM.td(null, "Product ID:"),
-                        React.DOM.td({
-                            className: "edid-content-data"
-                        }, this.edidParser.getProductCode())
-                    ),
-                    React.DOM.tr(null,
-                        React.DOM.td(null, "Serial Number:"),
-                        React.DOM.td({
-                            className: "edid-content-data"
-                        }, this.edidParser.getSerialNumber())
-                    ),
-                    React.DOM.tr(null,
-                        React.DOM.td(null, "Week:"),
-                        React.DOM.td({
-                            className: "edid-content-data"
-                        }, this.edidParser.getManufactureWeek())
-                    ),
-                    React.DOM.tr(null,
-                        React.DOM.td(null, "Year:"),
-                        React.DOM.td({
-                            className: "edid-content-data"
-                        }, this.edidParser.getManufactureYear())
-                    )
-                )
-            )
+            React.DOM.div(null, this.manufacturerInfo())
         );
     }
 });
