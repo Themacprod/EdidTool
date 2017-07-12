5395b8fdc4c93e9449a5163925b7d40380c784cd
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2017-07-12 12:14:21 -0400

    Refactore manufacturer structure Use div instead of table

--- react_components/edidcontent.js
+++ react_components/edidcontent.js
@@ -9,50 +9,30 @@ module.exports = React.createClass({
     componentWillMount: function() {
         this.edidParser = new EdidParser();
     },
+    contentSubGroup: function(detail, data) {
+        return React.DOM.div({
+                className: "edid-content-subgroup"
+            }, React.DOM.div({
+                className: "subgroup-detail"
+            }, detail),
+            React.DOM.div({
+                className: "subgroup-data"
+            }, data)
+        );
+    },
     manufacturerInfo: function() {
-        return React.DOM.table(null,
-            React.DOM.tbody(null,
-                React.DOM.tr(null,
-                    React.DOM.th(null, "Manufacturer & Product ID"),
-                    React.DOM.th(null, "")
-                ),
-                React.DOM.tr(null,
-                    React.DOM.td(null, "Manufacturer ID (PnPID):"),
-                    React.DOM.td({
-                        className: "edid-content-data"
-                    }, this.edidParser.getManufacturerId())
-                ),
-                React.DOM.tr(null,
-                    React.DOM.td(null, "Manufacturer name:"),
-                    React.DOM.td({
-                        className: "edid-content-data"
-                    }, this.edidParser.getManufacturerName())
-                ),
-                React.DOM.tr(null,
-                    React.DOM.td(null, "Product ID:"),
-                    React.DOM.td({
-                        className: "edid-content-data"
-                    }, this.edidParser.getProductCode())
-                ),
-                React.DOM.tr(null,
-                    React.DOM.td(null, "Serial Number:"),
-                    React.DOM.td({
-                        className: "edid-content-data"
-                    }, this.edidParser.getSerialNumber())
-                ),
-                React.DOM.tr(null,
-                    React.DOM.td(null, "Week:"),
-                    React.DOM.td({
-                        className: "edid-content-data"
-                    }, this.edidParser.getManufactureWeek())
-                ),
-                React.DOM.tr(null,
-                    React.DOM.td(null, "Year:"),
-                    React.DOM.td({
-                        className: "edid-content-data"
-                    }, this.edidParser.getManufactureYear())
-                )
-            )
+        return React.DOM.div({
+                className: "edid-content-group"
+            },
+            React.DOM.div({
+                className: "edid-content-title"
+            }, "Manufacturer & Product ID"),
+            this.contentSubGroup("Manufacturer ID (PnPID):", this.edidParser.getManufacturerId()),
+            this.contentSubGroup("Manufacturer name:", this.edidParser.getManufacturerName()),
+            this.contentSubGroup("Product ID:", this.edidParser.getProductCode()),
+            this.contentSubGroup("Serial Number:", this.edidParser.getSerialNumber()),
+            this.contentSubGroup("Week:", this.edidParser.getManufactureWeek()),
+            this.contentSubGroup("Year:", this.edidParser.getManufactureYear())
         );
     },
     render: function() {
