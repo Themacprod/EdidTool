cac438d27e410acf58d57f86070e99ea27bc77e4
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2017-09-13 15:14:34 -0400

    Removed unused include + beautify code

--- react_components/edidcontent-func.js
+++ react_components/edidcontent-func.js
@@ -2,53 +2,52 @@
 
 "use strict";
 
-var React = require("react"),
-  EdidParser = require("./edidparser");
+var React = require("react");
 
 var contentSubGroup = function(detail, data) {
-  return React.DOM.div({
-      className: "edid-content-subgroup"
-    }, React.DOM.div({
-      className: "subgroup-detail"
-    }, detail),
-    React.DOM.div({
-      className: "subgroup-data"
-    }, data)
-  );
+    return React.DOM.div({
+            className: "edid-content-subgroup"
+        }, React.DOM.div({
+            className: "subgroup-detail"
+        }, detail),
+        React.DOM.div({
+            className: "subgroup-data"
+        }, data)
+    );
 };
 
 var contentSubGroupRadio = function(detail, checked) {
-  return React.DOM.div({
-      className: "edid-content-subgroup"
-    }, React.DOM.div({
-      className: "subgroup-radio"
-    }, React.DOM.input({
-      type: "radio",
-      readOnly: "readOnly",
-      checked: checked
-    })),
-    React.DOM.div({
-      className: "subgroup-data"
-    }, detail));
+    return React.DOM.div({
+            className: "edid-content-subgroup"
+        }, React.DOM.div({
+            className: "subgroup-radio"
+        }, React.DOM.input({
+            type: "radio",
+            readOnly: "readOnly",
+            checked: checked
+        })),
+        React.DOM.div({
+            className: "subgroup-data"
+        }, detail));
 };
 
 var contentSubGroupCheckbox = function(detail, checked) {
-  return React.DOM.div({
-      className: "edid-content-subgroup"
-    }, React.DOM.div({
-      className: "subgroup-checkbox"
-    }, React.DOM.input({
-      type: "checkbox",
-      readOnly: "readOnly",
-      checked: checked
-    })),
-    React.DOM.div({
-      className: "subgroup-data"
-    }, detail));
+    return React.DOM.div({
+            className: "edid-content-subgroup"
+        }, React.DOM.div({
+            className: "subgroup-checkbox"
+        }, React.DOM.input({
+            type: "checkbox",
+            readOnly: "readOnly",
+            checked: checked
+        })),
+        React.DOM.div({
+            className: "subgroup-data"
+        }, detail));
 };
 
 module.exports = {
-  contentSubGroup: contentSubGroup,
-  contentSubGroupRadio: contentSubGroupRadio,
-  contentSubGroupCheckbox: contentSubGroupCheckbox
+    contentSubGroup: contentSubGroup,
+    contentSubGroupRadio: contentSubGroupRadio,
+    contentSubGroupCheckbox: contentSubGroupCheckbox
 };
