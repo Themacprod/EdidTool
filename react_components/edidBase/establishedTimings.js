a8c04c74c3edc9983ba160e3f95a13df4558e5d0
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2018-02-23 22:51:46 -0500

    Moved established timings to specific file.

---
+++ react_components/edidBase/establishedTimings.js
@@ -0,0 +1,50 @@
+/* global module:true */
+
+"use strict";
+
+var React = require("react"),
+    Func = require("../edidcontent-func"),
+    Title = require("./edidContentTitle"),
+    _ = require("lodash");
+
+module.exports = React.createClass({
+    render: function() {
+        return React.DOM.div({
+                className: "edid-content-group"
+            },
+            React.createElement(Title, {
+                title: "Established timings"
+            }),
+            _.map(this.props.establishedTimingsGroups, _.bind(function(establishedTimingsGroup, key) {
+                return React.DOM.div({
+                        className: "edid-content-column width-33",
+                        key: key
+                    },
+                    React.DOM.div({
+                            className: "edid-content-established",
+                            key: key
+                        },
+                        React.DOM.div(
+                            null,
+                            React.DOM.strong(
+                                null,
+                                establishedTimingsGroup.description
+                            )
+                        ),
+                        React.DOM.div({
+                                className: "edid-content-established"
+                            },
+                            _.map(establishedTimingsGroup, _.bind(function(establishedTimings, keygroup) {
+                                return Func.contentSubGroupCheckboxKey(
+                                    establishedTimings.hactive + "x" + establishedTimings.vactive + " @ " + establishedTimings.refresh + " Hz [" + establishedTimings.description + "]",
+                                    establishedTimings.checked,
+                                    keygroup
+                                );
+                            }, this))
+                        )
+                    )
+                );
+            }, this))
+        );
+    }
+});
--- react_components/edidcontentbase.js
+++ react_components/edidcontentbase.js
@@ -5,6 +5,7 @@
 var React = require("react"),
     Func = require("./edidcontent-func"),
     ManufacturerInfo = require("./edidBase/manufacturerInfo"),
+    EstablishedTimings = require("./edidBase/establishedTimings"),
     StandardTimings = require("./edidBase/standardTimings"),
     VideoInputDefinition = require("./edidBase/videoInputDefinition"),
     DisplayTransfertCharacteristics = require("./edidBase/displayTransfertCharacteristics"),
@@ -47,47 +48,6 @@ module.exports = React.createClass({
             }, React.DOM.strong(null, "Feature Support"))
         );
     },
-    establishedTimings: function() {
-        var establishedTimingsGroups = this.props.edidParsed.getEstablishedModes();
-
-        return React.DOM.div({
-                className: "edid-content-group"
-            },
-            React.DOM.div({
-                className: "edid-content-title"
-            }, React.DOM.strong(null, "Established timings")),
-            _.map(establishedTimingsGroups, _.bind(function(establishedTimingsGroup, key) {
-                return React.DOM.div({
-                        className: "edid-content-column width-33",
-                        key: key
-                    },
-                    React.DOM.div({
-                            className: "edid-content-established",
-                            key: key
-                        },
-                        React.DOM.div(
-                            null,
-                            React.DOM.strong(
-                                null,
-                                establishedTimingsGroup.description
-                            )
-                        ),
-                        React.DOM.div({
-                                className: "edid-content-established"
-                            },
-                            _.map(establishedTimingsGroup, _.bind(function(establishedTimings, keygroup) {
-                                return Func.contentSubGroupCheckboxKey(
-                                    establishedTimings.hactive + "x" + establishedTimings.vactive + " @ " + establishedTimings.refresh + " Hz [" + establishedTimings.description + "]",
-                                    establishedTimings.checked,
-                                    keygroup
-                                );
-                            }, this))
-                        )
-                    )
-                );
-            }, this))
-        );
-    },
     render: function() {
         return React.DOM.div(
             null,
@@ -102,7 +62,9 @@ module.exports = React.createClass({
             }),
             React.DOM.div(null, this.screenSize()),
             React.DOM.div(null, this.featureSupport()),
-            React.DOM.div(null, this.establishedTimings()),
+            React.createElement(EstablishedTimings, {
+                establishedTimingsGroups: this.props.edidParsed.getEstablishedModes()
+            }),
             React.createElement(StandardTimings, {
                 standardTimings: this.props.edidParsed.getStandardDisplayModes()
             })
