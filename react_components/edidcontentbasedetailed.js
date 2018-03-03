06b93494e389da67b90b21af01af18b03c99134a
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2018-03-02 22:01:33 -0500

    Removed debug console.log

--- react_components/edidcontentbasedetailed.js
+++ react_components/edidcontentbasedetailed.js
@@ -8,12 +8,9 @@ var React = require("react"),
 
 module.exports = React.createClass({
     render: function() {
-        var dtds = this.props.edidParsed.getDtds();
-        console.log(dtds);
-
         return React.DOM.div(
             null,
-            _.map(dtds.dtdType, function(block, key) {
+            _.map(this.props.edidParsed.getDtds().dtdType, function(block, key) {
                 return React.createElement(DetailedDataBlock, {
                     key: key,
                     title: "Block " + String(key + 1),
