cfe16440469a536cdb2376a48edda7be56f30087
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2018-04-03 22:50:30 -0400

    Nes files

---
+++ react_components/edidParser/edidBase/basicDisplayParameters/screenSizeAspectRatio.js
@@ -0,0 +1,38 @@
+/**
+ * Gets horizontal and vertical screen size or aspect ratio
+ * @param {array} edidData Byte array filled with EDID content.
+ * @returns {int} Gamma value.
+ */
+module.exports.getScreenSizeAspectRatio = function(edidData) {
+    const OFFSET_DATA_1 = 0x15;
+    const OFFSET_DATA_2 = 0x16;
+    var type = '';
+    var screenSize = {};
+    var aspectRatio = {};
+
+    if ((edidData[OFFSET_DATA_1] !== 0) && (edidData[OFFSET_DATA_2] !== 0)) {
+        type = 'ScreenSize';
+        screenSize.horizontalSize = this.edidData[OFFSET_DATA_1];
+        screenSize.verticalSize = this.edidData[OFFSET_DATA_2];
+    } else {
+        type = 'AspectRatio';
+
+        if ((edidData[OFFSET_DATA_1] === 0) && (edidData[OFFSET_DATA_2] === 0)) {
+            console.log('Invalid screen orientation');
+        }
+
+        if (edidData[OFFSET_DATA_1] !== 0) {
+            aspectRatio.portrait = false;
+        }
+
+        if (edidData[OFFSET_DATA_2] !== 0) {
+            aspectRatio.portrait = true;
+        }
+    }
+
+    return {
+        type: type,
+        screenSize: screenSize,
+        aspectRatio: aspectRatio
+    };
+};
