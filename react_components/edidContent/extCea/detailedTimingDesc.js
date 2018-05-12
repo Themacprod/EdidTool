00abd8056adce7246f84a140ff7cf16fd5fbebf1
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2018-05-11 22:20:51 -0400

    Moved DTD to tab,

--- react_components/edidContent/extCea/detailedTimingDesc.js
+++ react_components/edidContent/extCea/detailedTimingDesc.js
@@ -4,11 +4,57 @@ var React = require('react'),
     DetailedTiming = require('../edidBaseDetailedData/detailedTiming');
 
 module.exports = React.createClass({
-    render: function () {
+    getInitialState: function () {
+        return {
+            selected: 0
+        };
+    },
+    handleClick: function (key) {
+        this.setState({
+            selected: key
+        });
+    },
+    generateTabs: function () {
         const dtdCount = this.props.edidParsed.getNumberExtDtds(this.props.extIndex);
+
+        var dtdTab = [];
+
+        for (let i = 0; i < dtdCount; i += 1) {
+            const dtdIndex = i + 1;
+            dtdTab.push(`DTD ${dtdIndex}`);
+        }
+
+        return React.DOM.ul(
+            {
+                className: 'nav nav-tabs'
+            },
+            _.map(dtdTab, _.bind(function (data, key) {
+                var baseState = this.state.selected === key ? ' active' : ' disabled';
+
+                return React.DOM.li(
+                    {
+                        key: key,
+                        className: 'nav-item',
+                        onClick: this.handleClick.bind(this, key)
+                    },
+                    React.DOM.a({
+                        className: `nav-link ${baseState}`
+                    }, data)
+                );
+            }, this))
+        );
+    },
+    generateDtd: function () {
         const dtdTimings = this.props.edidParsed.getExtDtds(this.props.extIndex);
 
-
+        return React.DOM.div(
+            null,
+            React.createElement(DetailedTiming, {
+                data: dtdTimings[this.state.selected]
+            })
+        );
+    },
+    render: function () {
         return React.DOM.div(
             {
                 className: 'edid-content-group cea-detailed-timing'
@@ -16,19 +62,8 @@ module.exports = React.createClass({
             React.createElement(Title, {
                 title: 'Detailed Timings Descriptor'
             }),
-            React.DOM.div(
-                null,
-                `DTD count :${dtdCount}`
-            ),
-            React.DOM.div(
-                null,
-                _.map(dtdTimings, (dtdTiming, key) => (
-                    React.createElement(DetailedTiming, {
-                        key: key,
-                        data: dtdTiming
-                    })
-                ))
-            )
+            this.generateTabs(),
+            this.generateDtd()
         );
     }
 });
